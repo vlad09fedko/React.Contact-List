2f9e861a9794e513b04118be4b45ff74be1800b8
foramer <vlad09fedko@gmail.com>
2026-08-02 17:37:49 +0300

    Add only light theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 import store from './store';
 
@@ -8,10 +9,15 @@ import App from './App';
 
 import './index.css';
 
+const lightTheme = createTheme({ pallete: { mode: 'light' } });
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
